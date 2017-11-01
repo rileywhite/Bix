1536965e39855d7560a6d15d281096ff37413c4f
Riley White <rileywhite@gmail.com>
2017-11-01 18:03:36 -0500

    Add license headers and fix some naming

--- src/Bix/Repositories/EntityFramework/EntityFrameworkRepositoryException.cs
+++ src/Bix/Repositories/EntityFramework/EntityFrameworkRepositoryException.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/***************************************************************************/
+// Copyright 2013-2017 Riley White
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/***************************************************************************/
+
+using System;
 
 namespace Bix.Repositories.EntityFramework
 {
@@ -12,7 +28,7 @@ namespace Bix.Repositories.EntityFramework
         public EntityFrameworkRepositoryException(string message, Exception innerException) : base(message, innerException) { }
 
         // see https://github.com/dotnet/coreclr/issues/2715 for discussion on this
-        //private VelocityApiException(SerializationInfo info, StreamingContext context)
+        //private EntityFrameworkRepositoryException(SerializationInfo info, StreamingContext context)
         //{
         //    this.LogInstance = Guid.Parse(info.GetString("LogInstance"));
         //}
