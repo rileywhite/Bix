83c763276fb12c435df21dd7d81d0b7708b96ef4
Riley White <rileywhite@gmail.com>
2017-11-01 17:59:40 -0500

    Add some tests

--- src/Bix/Core.Tests/BixExtensionsTest.cs
+++ src/Bix/Core.Tests/BixExtensionsTest.cs
@@ -1,4 +1,20 @@
-﻿using Newtonsoft.Json;
+﻿/***************************************************************************/
+// Copyright 2013-2017 Riley White
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/***************************************************************************/
+
+using Newtonsoft.Json;
 using AutoFixture;
 using System;
 using Xunit;
@@ -50,6 +66,52 @@ namespace Bix.Core
 
             // assert
             Assert.Equal(jsonWithIndentedSettings, json);
+        }
+
+        [Fact]
+        public void ConvertFromJsonFailsOnNull()
+        {
+            // arrange
+            string json = null;
+
+            // act & assert
+            Assert.Throws<ArgumentNullException>("source", () => json.ConvertFromJson<object>());
+        }
+
+        [Fact]
+        public void ConvertFromJsonReturnsNullForObjectTypeAndInvalidJson()
+        {
+            // arrange
+            string json = "";
+
+            // act
+            var value = json.ConvertFromJson<object>();
+
+            // assert
+            Assert.Null(value);
+        }
+
+        [Fact]
+        public void ConvertFromJsonFailsForValueTypeAndInvalidJson()
+        {
+            // arrange
+            string json = "";
+
+            // act & assert
+            Assert.Throws<JsonSerializationException>(() => json.ConvertFromJson<decimal>());
+        }
+
+        [Fact]
+        public void ConvertFromJsonWorksForObjectType()
+        {
+            // arrange
+            string json = "hello";
+
+            // act
+            var str = json.ConvertFromJson<string>();
+
+            // assert
+            Assert.Equal("hello", str);
         }
     }
 }
