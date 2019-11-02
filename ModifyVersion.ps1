25c25b9c7b177305e4bf7684b4a82fd3d8bb50db
Riley White <rileywhite@gmail.com>
2019-11-01 20:38:59 -0500

    Modify version pre-release indicator for pre-release and add metadata for release versions

--- ModifyVersion.ps1
+++ ModifyVersion.ps1
@@ -5,7 +5,7 @@ Param($projectFile, $buildNum)
 $content = [IO.File]::ReadAllText($projectFile)
 
 $regex = new-object System.Text.RegularExpressions.Regex (
-		'([\d]+.[\d]+.[\d](-[a-zA-Z\.\-0-9]*)?)<\/Version>', 
+		'(([\d]+.[\d]+.[\d])(-[a-zA-Z\.\-0-9]*)?)<\/Version>', 
          [System.Text.RegularExpressions.RegexOptions]::MultiLine)
 
 $version = $null
@@ -16,17 +16,32 @@ if(!$match.Success) { throw 'No version found' }
 $version = $match.groups[1].value
 echo "version = $version"
 
-if (![string]::IsNullOrEmpty($match.groups[2]))
+$numericPartOfVersion = $match.groups[2].value
+echo "numericPartOfVersion = $numericPartOfVersion"
+
+$fileVersion = "$numericPartOfVersion.$buildNum"
+echo "fileVersion = $fileVersion"
+
+if ([string]::IsNullOrEmpty($match.groups[3]))
+{
+	# found a release indicator
+	# append buildNum and timestamp to the version as metadata
+	$buildTimestamp = (Get-Date).ToUniversalTime().ToString("yyyyMMddTHHmmssfffffffZ")
+	$version = "$version+build.$buildNum-$buildTimestamp"
+	echo "release version detected: updated with buildNum to $version"
+}
+else
 {
 	# found a pre-release indicator
-	# insert "+[build_number]" before closing Version tag
-	$version = "$version+build.$buildNum"
-echo "version now $version"
-	$content = $regex.Replace($content, "$version</Version>")
-
-	# update csproj file
-	[IO.File]::WriteAllText($projectFile, $content)
+	# append buildNum to the pre-release portion of the version
+	$version = "$version-build.$buildNum"
+	echo "pre-release version detected: updated with buildNum to $version"
 }
+
+$content = $regex.Replace($content, "$version</Version><FileVersion>$fileVersion</FileVersion>")
+
+# update csproj file
+[IO.File]::WriteAllText($projectFile, $content)
 
 echo "updating AppVeyor version"
 
