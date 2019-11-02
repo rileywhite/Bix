5b309195e850fe3505aca195655df27863d62c4f
Riley White <rileywhite@gmail.com>
2019-11-01 19:17:39 -0500

    Update versioning to better follow semver 2.0 with build metadata appended to end for prerelease

--- ModifyVersion.ps1
+++ ModifyVersion.ps1
@@ -4,25 +4,31 @@ Param($projectFile, $buildNum)
 
 $content = [IO.File]::ReadAllText($projectFile)
 
-$regex = new-object System.Text.RegularExpressions.Regex ('()([\d]+.[\d]+.[\d]+)(.[\d]+)((-[a-zA-Z\.\-0-9]*)?<\/Version>)', 
+$regex = new-object System.Text.RegularExpressions.Regex (
+		'([\d]+.[\d]+.[\d](-[a-zA-Z\.\-0-9]*)?)<\/Version>', 
          [System.Text.RegularExpressions.RegexOptions]::MultiLine)
 
 $version = $null
 $match = $regex.Match($content)
 echo $match
-if($match.Success) {
-    # from "1.0.0.0" this will extract "1.0.0"
-    $version = $match.groups[1].value
+if(!$match.Success) { throw 'No version found' }
+
+$version = $match.groups[1].value
+echo "version = $version"
+
+if (![string]::IsNullOrEmpty($match.groups[2]))
+{
+	# found a pre-release indicator
+	# insert "+[build_number]" before closing Version tag
+	$version = "$version+build.$buildNum"
+echo "version now $version"
+	$content = $regex.Replace($content, "$version</Version>")
+
+	# update csproj file
+	[IO.File]::WriteAllText($projectFile, $content)
 }
 
-# suffix build number onto $version. eg "1.0.0.15"
-$version = "$version.$buildNum"
-
-# update "1.0.0.0" to "$version"
-$content = $regex.Replace($content, '${2}' + $version + '${4}')
-
-# update csproj file
-[IO.File]::WriteAllText($projectFile, $content)
+echo "updating AppVeyor version"
 
 # update AppVeyor build
 Update-AppveyorBuild -Version $version
